8a8e3f18db17a5c8887e6b555822e453d88bee1e
PieApples <31902568+PieApples@users.noreply.github.com>
2018-09-15 19:46:32 -0400

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -0,0 +1,87 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
+public class Drive extends OpMode
+{
+
+    private DcMotor leftFront = null;
+    private DcMotor rightFront = null;
+    private DcMotor leftBack = null;
+    private DcMotor rightBack = null;
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        leftFront  = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+
+    }
+
+
+    @Override
+    public void init_loop() {
+    }
+
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void loop() {
+
+        double leftPower;
+        double rightPower;
+
+
+        double drive = -gamepad1.left_stick_y;
+        double turn  =  gamepad1.right_stick_x;
+        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+
+        leftFront.setPower(leftPower);
+        rightFront.setPower(rightPower);
+        leftBack.setPower(leftPower);
+        rightBack.setPower(rightPower);
+
+        double leftRight = gamepad1.left_stick_x;
+
+
+
+        if(leftRight > 0){
+            leftFront.setPower(1);
+            leftBack.setPower(-1);
+            rightFront.setPower(-1);
+            rightBack.setPower(1);
+
+        }
+
+        if(leftRight > 0){
+            leftFront.setPower(-1);
+            leftBack.setPower(1);
+            rightFront.setPower(1);
+            rightBack.setPower(-1);
+        }
+
+    }
+
+    @Override
+    public void stop() {
+    }
+
+}
